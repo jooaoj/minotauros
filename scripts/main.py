25986c85c6b53f0c493fe2306a2b8f12cc2c83d2
Jooa Jaakkola <jaakkola.jooa@gmail.com>
2025-05-22 07:59:26 +0300

    python 2d-list

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,24 @@
+#import
+
+MAZE1 = 'maze-task-first.txt'
+MAZE2 = 'maze-task-second.txt'
+
+def prepMaze():
+	maze = []
+
+	print("prepMaze")
+	with open(MAZE1) as fp:
+		for line in fp:
+			tmp = list(line)
+			maze.append(tmp)
+	print(maze[18][18])
+
 
 def main():
+	print("main")
+	prepMaze()
 	return 0
 
-if '__name__' == '__main__':
-	main()
+#if '__name__' == '__main__':
+print("starting...")
+main()
