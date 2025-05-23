2b8241c1baa87189c8c307be04e3b7e6c08ed508
Jooa Jaakkola <jaakkola.jooa@gmail.com>
2025-05-23 13:34:21 +0300

    Finding starting position

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,8 +1,29 @@
+use std::error::Error;
 
-pub fn simple_r(maze: Vec<Vec<char>>) -> Result<(), ()> {
-	//println!("{:?}", maze);
+pub fn simple_r(maze: Vec<Vec<char>>) -> Result<(), Box<dyn Error>> {
+	//let mut src: Option<usize>;
+	let mut cur: Vec<usize> = Vec::new();
+	let mut dst: Vec<Option<usize>> = Vec::new();
 
-	
+	let mut found: bool = false;
+
+	// Scan maze
+	for (y, row) in maze.iter().enumerate() {
+		println!("{:?}", &row);
+		for (x, col) in row.iter().enumerate() {
+			if !found {
+				if let Some(src) = row.iter().position(|&caret| caret == '^') {
+					cur.push(Some(src).unwrap());
+					cur.push(y);
+					found = true;
+				}
+			} 
+			//print!("{:?}", &col);
+		}
+		//println!("\n");
+	}
+
+	println!("{:?}", cur);
 
 	Ok(())
 }
